74f3ad664229106f649b71b028b614ac85f67f83
VIKASH KUMAR <81161755+vikashkumar707@users.noreply.github.com>
2024-07-14 19:46:38 +0530

    Update variable.tf

--- module/variable.tf
+++ module/variable.tf
@@ -28,10 +28,7 @@ variable "region" {
   default     = null
 }
 
-variable "user_label" {
-  description = "A user-defined label for organizing resources"
-  type        = string
-}
+
 
 variable "timeouts" {
   description = "nested block: NestingSingle, min items: 0, max items: 0"
