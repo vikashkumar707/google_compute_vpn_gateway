8385046ae2c4d8503a9d924ffbd5af6038d02d69
VIKASH KUMAR <81161755+vikashkumar707@users.noreply.github.com>
2024-07-14 18:51:59 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,38 +1,7 @@
 resource "google_compute_network" "vpc_network" {
-  project                 = var.project
-  name                    = var.vpc_name
-  auto_create_subnetworks = var.auto_create_value
-  mtu                     = var.mtu
-  description             = var.description
-  routing_mode            = var.routing_mode
-  delete_default_routes_on_create = var.delete_default_value
-
-timeouts {
-    create = var.timecreate   
-    delete = var.timedelete  
-    update = var.timeupdate   
-  }
-}
-
-resource "google_compute_subnetwork" "custom_subnet" {
-  project       = var.project
-  name          = var.subnet_name
-  ip_cidr_range = var.ip_cidr_range
-  region        = var.region
-  network       = google_compute_network.vpc_network.name
-  private_ip_google_access     = var.private_ip_google_access
-  private_ipv6_google_access   = var.private_ipv6_google_access
-
-  secondary_ip_range {
-    range_name    = var.range_name
-    ip_cidr_range = var.ip_cidr_range1
-  }
-
-  log_config {
-    aggregation_interval = var.aggregation_interval
-    flow_sampling        = var.flow_sampling
-    metadata             = var.metadata
-  }
+  name                    = var.network
+  auto_create_subnetworks = true
+  project                 = var.project  # Ensure project is specified
 }
 
 resource "google_compute_vpn_gateway" "this" {
