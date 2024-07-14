619d091102a7e1b9c0dd04b05d16c1be22bb38f8
VIKASH KUMAR <81161755+vikashkumar707@users.noreply.github.com>
2024-07-14 18:45:15 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -41,7 +41,7 @@ resource "google_compute_vpn_gateway" "this" {
   # name - (required) is a type of string
   name = var.name
   # network - (required) is a type of string
-  network = var.network
+  network = var.vpc_name
   # project - (optional) is a type of string
   project = var.project
   # region - (optional) is a type of string
