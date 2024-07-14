3f2d34c0c45bc6279a2a4d7ec5aceeeac860d0fc
VIKASH KUMAR <81161755+vikashkumar707@users.noreply.github.com>
2024-07-14 17:12:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "google_compute_vpn_gateway" {
+  source =  "./module"
+
+  # description - (optional) is a type of string
+  description = null
+  # name - (required) is a type of string
+  name = null
+  # network - (required) is a type of string
+  network = null
+  # project - (optional) is a type of string
+  project = null
+  # region - (optional) is a type of string
+  region = null
+
+  timeouts = [{
+    create = null
+    delete = null
+  }]
+}
