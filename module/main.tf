233fd52060ff7fb2b8cc8b38ad5530ce32e7d6b6
VIKASH KUMAR <81161755+vikashkumar707@users.noreply.github.com>
2024-07-14 17:07:50 +0530

    Create main.tf

---
+++ module/main.tf
@@ -0,0 +1,23 @@
+resource "google_compute_vpn_gateway" "this" {
+  # description - (optional) is a type of string
+  description = var.description
+  # name - (required) is a type of string
+  name = var.name
+  # network - (required) is a type of string
+  network = var.network
+  # project - (optional) is a type of string
+  project = var.project
+  # region - (optional) is a type of string
+  region = var.region
+
+  dynamic "timeouts" {
+    for_each = var.timeouts
+    content {
+      # create - (optional) is a type of string
+      create = timeouts.value["create"]
+      # delete - (optional) is a type of string
+      delete = timeouts.value["delete"]
+    }
+  }
+
+}
