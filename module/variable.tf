d3a1c7e21dd305fe8874c90e33c43457a4186b03
VIKASH KUMAR <81161755+vikashkumar707@users.noreply.github.com>
2024-07-14 18:43:27 +0530

    Update variable.tf

--- module/variable.tf
+++ module/variable.tf
@@ -1,3 +1,109 @@
+# REQUIRED VARIABLES
+variable "vpc_name" {
+  type        = string
+  description = "VPC name"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Subnet name"
+}
+
+# OPTIONAL VARIABLES
+
+variable "project" {
+  type        = string
+  description = "Project ID"
+}
+
+variable "auto_create_value" {
+  type        = bool
+  description = "Auto create subnetworks"
+}
+
+variable "mtu" {
+  type        = number
+  description = "MTU size"
+}
+
+variable "routing_mode" {
+  type        = string
+  description = "Routing mode"
+}
+
+variable "description" {
+  type        = string
+  description = "Description"
+}
+
+variable "delete_default_value" {
+  type        = bool
+  description = "Delete default routes"
+}
+
+variable "ip_cidr_range" {
+  type        = string
+  description = "IP CIDR range"
+}
+
+variable "region" {
+  type        = string
+  description = "Region"
+}
+
+variable "timecreate" {
+  type        = string
+  description = "Create timeout"
+}
+
+variable "timedelete" {
+  type        = string
+  description = "Delete timeout"
+}
+
+variable "timeupdate" {
+  type        = string
+  description = "Update timeout"
+}
+
+variable "aggregation_interval" {
+  description = "Aggregation interval"
+  type        = string
+}
+
+variable "flow_sampling" {
+  description = "Flow sampling"
+  type        = string
+}
+
+variable "metadata" {
+  description = "Metadata"
+  type        = string
+}
+
+variable "ip_cidr_range1" {
+  description = "Secondary IP CIDR range"
+  type        = string
+}
+
+variable "range_name" {
+  description = "Secondary IP range name"
+  type        = string
+}
+
+variable "private_ip_google_access" {
+  description = "private_ip_google_access"
+  type        = bool
+  default     = false
+}
+
+variable "private_ipv6_google_access" {
+  description = "private_ipv6_google_access"
+  type        = bool
+  default     = false
+}
+
+
 variable "description" {
   description = "(optional) - An optional description of this resource."
   type        = string
