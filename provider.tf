bb8c64d937cef9ab60acf42b6d74f8d4e21eb115
VIKASH KUMAR <81161755+vikashkumar707@users.noreply.github.com>
2024-07-14 19:09:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,16 @@
 terraform {
   required_providers {
-    google = ">= 3.63.0"
+    google = {
+      source = "hashicorp/google"
+      version = "5.30.0"
+    }
   }
+}
+
+provider "google" {
+  project = "ceq-devops-sandbox"
+}
+terraform{
+  backend "gcs" {}
+
 }
